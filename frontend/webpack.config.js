7c94568c78e86675ce59a382eaf8f02e77eb18de
Dung Tran <pdung1989@gmail.com>
2020-10-23 00:01:56 +0300

    fix packge.json

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 var webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
     template: __dirname + '/app/index.html',
